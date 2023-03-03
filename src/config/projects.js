08f0ddf79f05b7e0f275ab9361a669aa6ff2062d
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2023-03-03 15:45:03 -0300

    Add config javascript

---
+++ src/config/projects.js
@@ -0,0 +1,22 @@
+export const PROJECTS_INFO = [
+  {
+    id: '36',
+    name: 'Proaco',
+    nota: 'Hacer uso de la funcion findRelsPropsAsOldWay'
+  },
+  {
+    id: '37',
+    name: 'BBOSH',
+    nota: 'Hacer uso de la funcion findRelsProps'
+  },
+  {
+    id: '28',
+    name: 'Pacasmayo',
+    nota: 'Hacer uso de la funcion findRelsPropsAsOldWay'
+  },
+  {
+    id: '30',
+    name: 'EBCO',
+    nota: 'Hacer uso de multilabels'
+  }
+]
