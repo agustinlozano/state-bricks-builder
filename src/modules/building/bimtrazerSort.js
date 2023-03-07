014fe756f5483890ef35a819f620d541dac11595
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2023-03-07 18:35:00 -0300

    Update BimtrazerSort process

--- src/modules/building/bimtrazerSort.js
+++ src/modules/building/bimtrazerSort.js
@@ -1,62 +1,86 @@
-import { getBlockCodes, storeBlocks } from '../../services/allservices'
 import {
-  normalizeGroundFloor,
-  parseUnicode,
-  checkException
+  getBlockCodes,
+  storeBlocks,
+  postDescriptionMap
+} from '../../services/allservices'
+import {
+  checkException,
+  checkBlocksHaveCode,
+  checkElementsHaveProjectId
 } from '../../utils/'
 import { getProjectGuid } from '../getting/'
 import { buildBlocks, prebuildBlocks } from './builders'
 import {
-  assignBlockCodes,
+  // collectDataFromBricks,
+  mergeBlocks,
   assignProjectId,
-  checkElementsHaveProjectId,
-  checkBlocksHaveCode,
+  assignBlockCodes,
   filterValidBlocks,
-  mergeBlocks
+  joinBlocksWithSameDescription,
+  deleteDescriptionType,
+  pushBlockCodes,
+  getDescriptionTypes
 } from './builderUtils'
 
 export async function bimtrazerSort (modelID, btzPropSets, btzRelsProps, blocksInDb = []) {
   console.log('1. Bimtrazer sort production')
 
-  blocksInDb.forEach(block => {
-    block.BtzDescription = normalizeGroundFloor(
-      parseUnicode(block.BtzDescription).toUpperCase().trim()
-    )
-  })
-
   const prebuiltBlocks = prebuildBlocks(btzPropSets, btzRelsProps)
   console.log('2. Prebuilt block ready', prebuiltBlocks)
 
   const blocks = await buildBlocks(modelID, prebuiltBlocks)
-  console.log('2. Create block structure ready', blocks)
+  console.log('3. Create block structure ready', blocks)
 
-  // Avoid a block with BtzDescription empty
-  const newBlocks = filterValidBlocks(blocks)
+  const newBlocks = joinBlocksWithSameDescription(
+    filterValidBlocks(blocks)
+  )
 
   // Assign projectId to each element
-  const projectId = await getProjectGuid(modelID)
-  assignProjectId(newBlocks, projectId)
-  checkElementsHaveProjectId(newBlocks)
+  await handleProjectId(newBlocks, modelID)
 
-  // Filter blocks with BtzCode === 'UNASSIGNED'
-  const codes = await getBlockCodes(newBlocks.length)
+  // Assign block codes to each block
+  await handleBlockCodes(newBlocks)
+
+  const typeOfDescriptions = getDescriptionTypes(newBlocks)
+  pushBlockCodes(newBlocks, typeOfDescriptions)
+
+  console.log('5. Type of descriptions', typeOfDescriptions)
+
+  // Call the API to set the new blocks on descriptionMap table
+  const mapDescriptionRes = await postDescriptionMap(typeOfDescriptions)
+
+  console.log('6. Response from DescriptionMap', mapDescriptionRes)
+
+  // Now, we have to delete the descriptionType from the blocks structure
+  const readyNewBlocks = deleteDescriptionType(newBlocks)
+
+  console.log('7. New blocks ready', readyNewBlocks)
+
+  // Merge new blocks with blocks in db
+  const finalBlocks = mergeBlocks(readyNewBlocks, blocksInDb)
+
+  console.log('8. Final blocks', finalBlocks)
+
+  const blockIfcRes = await storeBlocks(finalBlocks, 'BlocksIFC')
+
+  console.log('9. Response from BlocksIFC', blockIfcRes)
+
+  return finalBlocks
+}
+
+export async function handleProjectId (blocks, modelID) {
+  const projectId = await getProjectGuid(modelID)
+
+  assignProjectId(blocks, projectId)
+  checkElementsHaveProjectId(blocks)
+}
+
+export async function handleBlockCodes (blocks) {
+  const codes = await getBlockCodes(blocks.length)
+  console.log('4. Block codes', codes)
 
   checkException(codes, 'There was an error getting the BtzCodes from the API.')
 
-  // Assign BtzCode to each block
-  assignBlockCodes(newBlocks, codes)
-  checkBlocksHaveCode(newBlocks)
-
-  console.log('3. New blocks ready', newBlocks)
-
-  // Merge new blocks with blocks in db
-  const finalBlocks = mergeBlocks(newBlocks, blocksInDb)
-
-  console.log('4. Final blocks', finalBlocks)
-
-  const response = await storeBlocks(finalBlocks, 'BlocksIFC')
-
-  console.log('5. Response from Bimtrazer', response)
-
-  return finalBlocks
+  assignBlockCodes(blocks, codes)
+  checkBlocksHaveCode(blocks)
 }
